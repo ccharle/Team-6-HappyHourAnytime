dabe34af19cac22e03c6a8feb32a46e05e1467d4
flyelizabethsisi <eyid1026@gmail.com>
2019-04-27 17:49:50 -0400

    clean up commit

--- app/src/main/java/org/pursuit/team_6_happyhouranytime/MainActivity.java
+++ app/src/main/java/org/pursuit/team_6_happyhouranytime/MainActivity.java
@@ -14,7 +14,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class  MainActivity extends AppCompatActivity {
     private Button testCallButton;
     private EditText drinksText;
     private final String TAG = "Drink Response";

ee472af0fc6a849ce6fe81544a762448fd224b06
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-04-27 14:56:05 -0400

    main page and activity for the randomly selected beverage

--- app/src/main/java/org/pursuit/team_6_happyhouranytime/MainActivity.java
+++ app/src/main/java/org/pursuit/team_6_happyhouranytime/MainActivity.java
@@ -2,12 +2,49 @@ package org.pursuit.team_6_happyhouranytime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button seachByNameButton;
+    private Button seachByIngredientsButton;
+    private Button pickADrinkAtRandomButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seachByNameButton = findViewById(R.id.search_by_name_button);
+        seachByIngredientsButton = findViewById(R.id.search_by_ingredients_button);
+        pickADrinkAtRandomButton = findViewById(R.id.pick_random_drink_button);
+
+        seachByNameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        seachByIngredientsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
+
+        pickADrinkAtRandomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
+
+
+
 }

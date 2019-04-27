c97f063219fa2cf29c89b8dadaf95a60ad0449d3
InquisitiveMindHasToKnow <omarraymond@pursuit.org>
2019-04-27 17:26:45 -0400

    added dialog with official gov't warnings. Switched toasts to snackbars

--- app/src/main/java/org/pursuit/team_6_happyhouranytime/MainActivity.java
+++ app/src/main/java/org/pursuit/team_6_happyhouranytime/MainActivity.java
@@ -1,57 +1,63 @@
 package org.pursuit.team_6_happyhouranytime;
 
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import org.pursuit.team_6_happyhouranytime.models.DrinksResponse;
-import org.pursuit.team_6_happyhouranytime.network.RetrofitSingleton;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
-    private Button testCallButton;
-    private EditText drinksText;
-    private final String TAG = "Drink Response";
     private Button seachByNameButton;
     private Button seachByIngredientsButton;
     private Button pickADrinkAtRandomButton;
+    private AlertDialog.Builder dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-      findViews();
-      
-        testCallButton.setOnClickListener(new View.OnClickListener() {
+        findViews();
+
+        dialog = new AlertDialog.Builder(MainActivity.this);
+        dialog.setCancelable(false);
+        dialog.setTitle("WARNING!!!");
+        dialog.setMessage("GOVERNMENT WARNING: (1) According to the Surgeon General, women should not drink alcoholic beverages during pregnancy because of the risk of birth defects. (2) Consumption of alcoholic beverages impairs your ability to drive a car or operate machinery, and may cause health problems.");
+        dialog.setPositiveButton("Acknowledge", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                String Drinks = drinksText.getText().toString().toLowerCase();
+            public void onClick(DialogInterface dialog1, int id) {
 
-                RetrofitSingleton.getInstance()
-                       // .getBartenderService().getDrinks(Drinks)
-                        .enqueue(new Callback<DrinksResponse>() {
-                            @Override
-                            public void onResponse(Call<DrinksResponse> call, Response<DrinksResponse> response) {
-                                Log.d(TAG, "OnResponse" + response.body());
-                            }
 
-                            @Override
-                            public void onFailure(Call<DrinksResponse> call, Throwable t) {
-                                Log.d(TAG, t.getMessage());
-                            }
-                        });
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Happy Drinking!!!", Snackbar.LENGTH_LONG);
+                View view = snackbar.getView();
+                TextView tv = view.findViewById(android.support.design.R.id.snackbar_text);
+                tv.setTextColor(ContextCompat.getColor(MainActivity.this, R.color.snackColor));
+                tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                snackbar.show();
+            }
 
-private void findViews(){
-    testCallButton = findViewById(R.id.test_call_button);
-        drinksText = findViewById(R.id.enter_drinks);
+        })
+                .setNegativeButton("Don't Acknowledge", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
 
+                        finish();
+                        System.exit(0);
+                    }
+                })
+
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
+
+    }
+
+    private void findViews() {
         seachByNameButton = findViewById(R.id.search_by_name_button);
         seachByIngredientsButton = findViewById(R.id.search_by_ingredients_button);
         pickADrinkAtRandomButton = findViewById(R.id.pick_random_drink_button);
@@ -70,23 +76,15 @@ private void findViews(){
             public void onClick(View v) {
 
 
-
             }
         });
-}
-  
-
-    }
-
-
 
         pickADrinkAtRandomButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-
             }
         });
     }
-
 }
+
